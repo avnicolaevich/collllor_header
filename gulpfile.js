a449b17294e8d37488c8c28b44b6b72e6c5f632e
avnicolaevich <avnicolaevich@gmail.com>
2017-05-05 15:58:47 +0300

    add search field

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var gulp           = require('gulp'),
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/magicsuggest/magicsuggest-min.js',
 		'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))

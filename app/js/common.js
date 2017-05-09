306fe40ab6b2231afa4ea9d0f600090a3b58d4d2
avnicolaevich <avnicolaevich@gmail.com>
2017-05-09 14:29:48 +0300

    responsive styles

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-	$('#magicsuggest').magicSuggest();
+	$('#magicsuggest').magicSuggest({
+        hideTrigger: true
+    });
 	
 	$(function () {
         $('.menu__sub').mouseover(function () {
@@ -9,6 +11,10 @@ $(document).ready(function(){
         $('.menu__sub').mouseout(function () {
             $('.invisibly').css("display", "none");
         });
-    })
+    });
+
+    $(".header__menu-button").click(function() {
+        $(".menu__hidden").toggle('slow');
+    });
 
 });

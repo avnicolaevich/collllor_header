f67428ca5c827f0a74ce05e359eb69740b10c1be
avnicolaevich <avnicolaevich@gmail.com>
2017-05-24 20:48:38 +0300

    refactor code

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
+
+	// SEARCH INPUT
 	$('#magicsuggest').magicSuggest({
         hideTrigger: true
     });
+	// SERCH INPUT END
 	
+	// SHADOW BLOCK
 	$(function () {
         $('.menu__sub').mouseover(function () {
             $('.invisibly').css("display", "block");
@@ -12,10 +16,12 @@ $(document).ready(function(){
             $('.invisibly').css("display", "none");
         });
     });
+	// SHADOW BLOCK END
 
+	//  OPEN MENU
     $(".header__nav-menu").click(function() {
         $(".menu__hidden").toggle('slow');
     });
-
+    // OPEN MENU END
 
 });
